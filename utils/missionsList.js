befee66a79e8605bf9d0a37dbf7a86c371d5948a
TitoSantos77 <147247853+TitoSantos77@users.noreply.github.com>
2026-05-01 22:46:12 +0100

    Create missionsList.js

---
+++ utils/missionsList.js
@@ -0,0 +1,17 @@
+export const MISSOES = [
+  {
+    id: 1,
+    descricao: "Faz 1 platina esta semana",
+    objetivo: { platinas: 1, conquistas: 0, xp: 0 },
+    recompensa: 50,
+    requerJogo: false
+  },
+  {
+    id: 2,
+    descricao: "Faz 3 conquistas",
+    objetivo: { platinas: 0, conquistas: 3, xp: 0 },
+    recompensa: 40,
+    requerJogo: false
+  },
+  // ... mete aqui TODAS as tuas missões antigas
+];
