b57dbf3cb2f09f8ff2aa71a0acd9a126f487b4a2
TitoSantos77 <147247853+TitoSantos77@users.noreply.github.com>
2026-05-02 10:49:35 +0100

    Create UserGames.js

---
+++ models/UserGames.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const platinaSchema = new mongoose.Schema({
+  jogo: String,
+  plataforma: String,
+  imagem: String,
+  data: { type: String, default: () => new Date().toISOString().split("T")[0] },
+  xpGanhos: Number
+});
+
+const conquistaSchema = new mongoose.Schema({
+  jogo: String,
+  plataforma: String,
+  imagem: String,
+  data: { type: String, default: () => new Date().toISOString().split("T")[0] },
+  xpGanhos: Number
+});
+
+const userGamesSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  platinas: { type: [platinaSchema], default: [] },
+  conquistas: { type: [conquistaSchema], default: [] }
+});
+
+export default mongoose.model("UserGames", userGamesSchema);
