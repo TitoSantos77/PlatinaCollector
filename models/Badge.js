d35a02415a88aa9aaf31b44b77f45d622c66d2fc
TitoSantos77 <147247853+TitoSantos77@users.noreply.github.com>
2026-06-19 14:14:22 +0100

    Update Badge.js

--- models/Badge.js
+++ models/Badge.js
@@ -1,16 +1,14 @@
-import User from "../models/User.js";
 import UserStats from "../models/UserStats.js";
 import Badge from "../models/Badge.js";
 
 export async function verificarBadges(userId) {
-  const user = await User.findOne({ userId });
   const stats = await UserStats.findOne({ userId });
   const badgesDB = await Badge.find();
 
-  if (!user) return false;
+  if (!stats) return false;
 
-  if (!user.badgesDesbloqueadas) {
-    user.badgesDesbloqueadas = [];
+  if (!stats.badgesDesbloqueadas) {
+    stats.badgesDesbloqueadas = [];
   }
 
   let ganhouNova = false;
@@ -19,37 +17,37 @@ export async function verificarBadges(userId) {
     const id = badge.id;
 
     // Já tem esta badge?
-    if (user.badgesDesbloqueadas.includes(id)) continue;
+    if (stats.badgesDesbloqueadas.includes(id)) continue;
 
     const req = badge.requisito || {};
 
-    const cumpreNivel = req.nivel ? user.nivel >= req.nivel : true;
-    const cumpreXP = req.xp ? user.totalXP >= req.xp : true;
+    const cumpreNivel = req.nivel ? stats.nivel >= req.nivel : true;
+    const cumpreXP = req.xp ? stats.totalXP >= req.xp : true;
 
     const cumprePlatinas = req.platinas
-      ? (stats?.platinas || 0) >= req.platinas
+      ? (stats.totalPlatinas || 0) >= req.platinas
       : true;
 
-    const cumpreConquistas = req.conquistas
-      ? (stats?.conquistas || 0) >= req.conquistas
+    const cumpreProezas = req.proezas
+      ? (stats.totalProezas || 0) >= req.proezas
       : true;
 
     const cumpreMissoes = req.missoes
-      ? (stats?.missoesConcluidas || 0) >= req.missoes
+      ? (stats.missoesConcluidas || 0) >= req.missoes
       : true;
 
     if (
       cumpreNivel &&
       cumpreXP &&
       cumprePlatinas &&
-      cumpreConquistas &&
+      cumpreProezas &&
       cumpreMissoes
     ) {
-      user.badgesDesbloqueadas.push(id);
+      stats.badgesDesbloqueadas.push(id);
       ganhouNova = true;
     }
   }
 
-  await user.save();
+  await stats.save();
   return ganhouNova;
 }
