597ab4442188a9a1a44c317f4608bb3ade7b2bed
TitoSantos77 <147247853+TitoSantos77@users.noreply.github.com>
2026-06-19 13:42:58 +0100

    Update resetall.js

--- commands/resetall.js
+++ commands/resetall.js
@@ -4,7 +4,7 @@ import { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } from "discord.
 import User from "../models/User.js";
 import UserStats from "../models/UserStats.js";
 import GlobalStats from "../models/GlobalStats.js";
-import UserGames from "../models/UserGames.js"; // <-- ADICIONADO
+import UserGames from "../models/UserGames.js";
 
 // BACKUP
 import { criarBackup } from "../utils/backup.js";
@@ -26,7 +26,7 @@ export async function execute(interaction) {
   await User.deleteMany({});
   await UserStats.deleteMany({});
   await GlobalStats.deleteMany({});
-  await UserGames.deleteMany({}); // <-- ADICIONADO
+  await UserGames.deleteMany({});
 
   // CRIAR BACKUP DEPOIS DO RESET GLOBAL
   criarBackup();
@@ -39,7 +39,7 @@ export async function execute(interaction) {
       "• XP e níveis\n" +
       "• Badges\n" +
       "• Platinas\n" +
-      "• Conquistas\n" +
+      "• Proezas\n" +
       "• Estatísticas globais\n" +
       "• Histórico completo\n\n" +
       "⚠️ Esta ação é permanente."
