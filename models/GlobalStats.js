ff292d53f4b2394a3abc5cdd13ee8aae5b95f18c
TitoSantos77 <147247853+TitoSantos77@users.noreply.github.com>
2026-07-02 11:24:06 +0100

    Update GlobalStats.js

--- models/GlobalStats.js
+++ models/GlobalStats.js
@@ -1,12 +1,34 @@
 import mongoose from "mongoose";
 
 const globalStatsSchema = new mongoose.Schema({
+  // Contagem de jogos (platinas)
   jogos: {
     type: Map,
     of: Number,
     default: {}
   },
+
+  // Contagem de plataformas (platinas)
   plataformas: {
+    type: Map,
+    of: Number,
+    default: {}
+  },
+
+  // 🟩 NOVO — CARREIRA GTA
+  categoriasCarreira: {
+    type: Map,
+    of: Number,
+    default: {}
+  },
+
+  subcategoriasCarreira: {
+    type: Map,
+    of: Number,
+    default: {}
+  },
+
+  plataformasCarreira: {
     type: Map,
     of: Number,
     default: {}
